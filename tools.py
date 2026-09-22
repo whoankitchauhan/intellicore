787053f9a0ec5fbe982e476ec36eeb45f67b2f8e
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-09-23 00:26:26 +0530

    Add scrape_url function to extract readable text from a specified URL

--- tools.py
+++ tools.py
@@ -1,4 +1,7 @@
+from pydoc import text
+
 from langchain.tools import tool
+from sqlalchemy import text
 import requests
 from bs4 import BeautifulSoup
 from tavily import TavilyClient
@@ -27,4 +30,18 @@ def web_search(query: str) -> str:
     except Exception as e:
         return f"An error occurred while searching: {str(e)}"
 
-print(web_search.invoke("latest advancements in AI technology"))
+@tool
+def scrape_url(url: str) -> str:
+    """Extract clean readable text from a specific website link. 
+    Use this ONLY when you already have a full target URL and need to read its content.
+    Do not use this for general search queries."""
+    try:
+        response = requests.get(url,timeout=8, headers={'User-Agent': 'Mozilla/5.0'})
+        response.raise_for_status()  # Raise an error for bad responses
+        soup = BeautifulSoup(response.content, 'html.parser')
+        for tag in soup(['script', 'style',"nav" , "footer"]):
+            tag.decompose()  # Remove script and style elements
+        text = soup.get_text(separator='\n', strip=True)
+        return soup.get_text(separator='\n', strip=True)[:3000]  # Return the first 3000 characters of the text
+    except Exception as e:
+        return f"An error occurred while scraping the URL: {str(e)}"
